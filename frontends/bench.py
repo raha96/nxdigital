d7232ddf06388b91e6f3642e892f27fe1fd2b565
RMS <raham9619@gmail.com>
2022-11-22 15:44:18 +0330

    Incremental

--- frontends/bench.py
+++ frontends/bench.py
@@ -1,6 +1,15 @@
 from .. import circuit
+import re
 
 def load_bench(bench:str) -> circuit.circuit():
     out = circuit.circuit()
-    print(bench)
+    
+    def process_ios(line:str) -> bool:
+        ismatch = re.findall(r"\b(([Ii][Nn][Pp][Uu][Tt])|([Oo][Uu][Tt][Pp][Uu][Tt]))\w*=\w*(", line)
+        print(ismatch)
+
+    with open(bench, "r") as benchin:
+        for line in benchin:
+            # Discard comments
+            line = line[:line.find("#")].strip()
     return out
