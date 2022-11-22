3ad56a6dee47e43894a1e28411f47a69c60cb4e7
RMS <raham9619@gmail.com>
2022-11-22 23:13:09 +0330

    Checkpoint on BENCH frontend; began Verilog

---
+++ frontends/verilog.py
@@ -0,0 +1,31 @@
+from .. import circuit, utils
+
+def dump_verilog(cir:circuit.circuit, filename:str, modulename="verilog_dump"):
+    def beautify(lst:list, perline:int=5, indent:str="    "*2) -> str:
+        lines = []
+        for i in range(0, len(lst), perline):
+            last = min(i + perline, len(lst))
+            lines.append(", ".join(lst[i:last]))
+        return (", \n" + indent).join(lines)
+    
+    indent = "    "
+
+    #fout = open(filename, "w")
+    inps, outs, nets = [], [], []
+    for net in cir.net_list:
+        ntype = cir.net_list[net].ntype
+        if ntype == utils._net_type.IN:
+            inps.append(net)
+        elif ntype == utils._net_type.OUT:
+            outs.append(net)
+        elif ntype == utils._net_type.INT:
+            nets.append(net)
+        else:
+            assert 0
+    
+    print(f"module {modulename} (" + (", ".join(inps)) + ", " + (", ".join(outs)) + ");" )
+    print(indent + "input " + beautify(inps) + ";\n")
+    print(indent + "output " + beautify(outs) + ";\n")
+    print(indent + "wire " + beautify(nets))
+    print("endmodule")
+    #fout.close()
