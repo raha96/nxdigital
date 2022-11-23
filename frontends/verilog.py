4e12655e9e3e179b82163a602bf4b6729b7a6b9c
RMS <raham9619@gmail.com>
2022-11-23 14:08:13 +0330

    Incremental

--- frontends/verilog.py
+++ frontends/verilog.py
@@ -58,7 +58,8 @@ def dump_verilog(cir:circuit.circuit, filename:str, modulename="verilog_dump"):
         node = cir.net_list[net]
         for adjnet in cir.graph.adj[node]:
             modules[adjnet.name].append(net)
-            print(f"{net} -> {adjnet}")
+            port = getport(net, module, cir, True)
+            print(f"{net} -> {adjnet}: {port}")
     #for module in cir.module_list:
     #    print(modules[module])
     print("endmodule")
