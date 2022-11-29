9057d9af4146e5ee74f904abd7c2c005e29fe64a
RMS <raham9619@gmail.com>
2022-11-29 12:48:25 +0330

    Edited __hash__ and implemented __eq__ and __ne__, turns out networkx relies on the latter two as well

--- utils.py
+++ utils.py
@@ -13,8 +13,14 @@ class _node_net():
     self.value = 'U'
     # U Z X 0 1
 
-  def __hash__(self) -> int:
-    return self.name.__hash__()
+  def __hash__(self):
+    return hash(self.name)
+  
+  def __eq__(self, other):
+    return (type(self) == type(other)) and (self.name == other.name)
+  
+  def __ne__(self, other):
+    return not self.__eq__(other)
   
   def __str__(self):
     return self.name
@@ -24,9 +30,15 @@ class _node_module():
   def __init__(self, name:str, mtype:str):
     self.name = name
     self.mtype = mtype
+
+  def __hash__(self):
+    return hash(self.name)
   
-  def __hash__(self) -> int:
-    return self.name.__hash__()
+  def __eq__(self, other):
+    return (type(self) == type(other)) and (self.name == other.name)
+  
+  def __ne__(self, other):
+    return not self.__eq__(other)
 
   def __str__(self):
     return self.name
