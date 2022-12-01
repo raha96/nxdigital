4b3ea28a41eda6424a3a1186daa69103e455a073
RMS <raham9619@gmail.com>
2022-12-01 13:20:44 +0330

    Bug fixes in reverse topological sort

--- search.py
+++ search.py
@@ -37,17 +37,17 @@ def topological_nets_from_outputs (cir:circuit.circuit) -> list:
                     if not marked[childnet]:
                         ready = False
                         break
-                if ready:
-                    q.put(parent)
-                    marked[parent] = True
+            if ready:
+                q.put(parent)
+                marked[parent] = True
     return order
 
 
 def topological_modules_from_outputs (cir:circuit.circuit) -> list:
     order_nets = topological_nets_from_outputs(cir)
     order = []
-    pred = cir.graph.pref
+    pred = cir.graph.pred
     for net in order_nets:
-        for mod in pred[net]:
+        for mod in pred[cir.net_list[net]]:
             order.append(mod.name)
     return order
