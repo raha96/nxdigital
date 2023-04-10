983b3902d4d1479a5196c94d0e7be648ebb92efc
RMS <raham9619@gmail.com>
2023-04-10 10:59:57 +0330

    Implemented validate_ports analyzers that checks to make sure all ports have been defined

---
+++ analysers/validate_ports.py
@@ -0,0 +1,11 @@
+from nxdigital.circuit import circuit
+
+def validate_ports(cir:circuit):
+    adj = cir.graph.adj
+    pred = cir.graph.pred
+    for netname in cir.net_list:
+        net = cir.net_list[netname]
+        for gate in adj[net]:
+            assert cir.get_port(net.name, gate.name) != None, f"Invalid port name for {net.name} -> {gate.name}"
+        for gate in pred[net]:
+            assert cir.get_port(gate.name, net.name) != None, f"Invalid port name for {gate.name} -> {net.name}"
